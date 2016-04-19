83473d968f172535bcb969ef3d479454081f0978
JiahuiGia <jiahuihu0425@gmail.com>
2016-04-19 15:07:25 -0700

    Update DealerManager.java

--- Final_Project/src/project/vehicle/management/dto/DealerManager.java
+++ Final_Project/src/project/vehicle/management/dto/DealerManager.java
@@ -8,10 +8,26 @@ package project.vehicle.management.dto;
 public class DealerManager {
 	
 	//DealerManager -> listOfDealers
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		// DealerManager -> listOfDealers
+	public List<String> getDealersName(File f) throws IOException {
+		BufferedReader br = null;
+		List<String> dealersNameList = new ArrayList<String>();
+		try {
+			br = new BufferedReader(new FileReader(f));
+			String nameLocWeb;
+			while ((nameLocWeb = br.readLine()) != null) {
+				String[] dealerInfo = nameLocWeb.split("	");
+				dealersNameList.add(dealerInfo[0]);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				br.close();
+			}
+		}
+		return dealersNameList;
+	}           
 }
